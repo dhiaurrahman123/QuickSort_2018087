31ae44fdcd24c5e42505f4a8c50434d43af2f740
dhiaurrahman123 <dhiaurrahman.i.ft18@mail.umy.ac.id>
2023-03-30 11:01:19 +0700

    membuat swap

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -26,5 +26,12 @@ void input() {
 		cout << "<" << (i + 1) << ">";
 		cin >> arr[i]; 
 	}
-
+}
+// swap the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp;
+	temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
 }
